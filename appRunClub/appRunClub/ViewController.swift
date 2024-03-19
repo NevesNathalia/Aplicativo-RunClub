16c4daa1542ecba528bd86163188db89d95389f3
Nathália <nathalianevesdasilva@2005>
2024-03-19 09:18:41 -0400

    create branch develop

---
+++ appRunClub/appRunClub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appRunClub
+//
+//  Created by Nathalia Neves on 19/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Teste")
+    }
+
+
+}
+
