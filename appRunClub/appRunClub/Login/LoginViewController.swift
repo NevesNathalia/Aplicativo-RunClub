eda3cfdebc1c66ef64b58fe1c067a897f71bfb28
Nathália <nathalianevesdasilva@2005>
2024-03-25 08:34:11 -0400

    lauchScreen foi criada

---
+++ appRunClub/appRunClub/Login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  appRunClub
+//
+//  Created by Nathalia Neves on 22/03/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
