585ac7899b578079bd681921738ac23bafd2aedb
Otavio Astrath Leite <otavioastrath00@gmail.com>
2024-03-29 12:41:38 +0100

    configured the labels and buttons

--- appRunClub/appRunClub/Login/LoginViewController.swift
+++ appRunClub/appRunClub/Login/LoginViewController.swift
@@ -8,11 +8,71 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var logInLabel: UILabel!
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var passwordForgotButton: UIButton!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    @IBOutlet weak var dontHaveAnAccountLabel: UILabel!
+    
+    @IBOutlet weak var toSignUpButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-        
+        configLabels()
+        configButtons()
+        configTextFields()
+    }
+    
+    
+    func configLabels() {
+        // Text
+        logInLabel.text = "Log In"
+        emailLabel.text = "Email"
+        passwordLabel.text = "Password"
+        // Colors
+        logInLabel.textColor = UIColor(red: 109/255, green: 181/255, blue: 139/255, alpha: 1.0)
+    }
+    
+    func configTextFields() {
+        // Corner Radius
+        emailTextField.layer.cornerRadius = 20.0
+        passwordTextField.layer.cornerRadius = 20.0
+    }
+    
+    func configButtons() {
+        //Setting Titles
+        passwordForgotButton.setTitle("Password forgotten?", for: .normal)
+        submitButton.setTitle("Log In", for: .normal)
+        toSignUpButton.setTitle("Sign Up", for: .normal)
+        // Colors
+        passwordForgotButton.setTitleColor(UIColor(red: 109/255, green: 181/255, blue: 139/255, alpha: 1.0), for: .normal)
+        submitButton.backgroundColor = UIColor(red: 109/255, green: 181/255, blue: 139/255, alpha: 1.0)
+        toSignUpButton.setTitleColor(UIColor(red: 109/255, green: 181/255, blue: 139/255, alpha: 1.0), for: .normal)
+        // Corner Radius
+        submitButton.layer.cornerRadius = 20.0
+            
+    }
+    
+    
+    @IBAction func tappedForgotPasswordButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func tappedSubmitButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func tappedToSignUpButton(_ sender: UIButton) {
     }
     
 
