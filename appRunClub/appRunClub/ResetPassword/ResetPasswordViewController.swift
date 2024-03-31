3c9d93bdd3ecfc004760ccc4b99080ca7cf94528
Nathália <nathalianevesdasilva@2005>
2024-03-31 17:02:20 -0400

    create branch resetPassword

---
+++ appRunClub/appRunClub/ResetPassword/ResetPasswordViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ResetPasswordViewController.swift
+//  appRunClub
+//
+//  Created by Nathalia Neves on 31/03/24.
+//
+
+import UIKit
+
+class ResetPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
