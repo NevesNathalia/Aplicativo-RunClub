dffb4efbad13c890b004fe38f25b9b7be9a08364
Otavio Astrath Leite <otavioastrath00@gmail.com>
2024-04-10 20:28:49 +0200

    created new screen for user searches

---
+++ appRunClub/appRunClub/userSearch/userSearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  userSearchViewController.swift
+//  appRunClub
+//
+//  Created by Otavio Astrath Leite on 10.04.24.
+//
+
+import UIKit
+
+class userSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+}
