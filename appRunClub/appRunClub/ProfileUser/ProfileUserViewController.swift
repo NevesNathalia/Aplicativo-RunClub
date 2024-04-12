e02e94097094bf517670c747c9b502ce9d53b3ca
Nathália <nathalianevesdasilva@2005>
2024-04-12 14:41:55 -0400

    create new branch profileUser

---
+++ appRunClub/appRunClub/ProfileUser/ProfileUserViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileUserViewController.swift
+//  appRunClub
+//
+//  Created by Nathalia Neves on 12/04/24.
+//
+
+import UIKit
+
+class ProfileUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
