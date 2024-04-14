d4f0e038893a9b29d31689b8bb56d83bbf074bfa
Nathália <nathalianevesdasilva@2005>
2024-04-13 23:44:04 -0400

    fazendo conexoes dos elementos

---
+++ appRunClub/appRunClub/editProfileUser/EditProfileUserViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EditProfileUserViewController.swift
+//  appRunClub
+//
+//  Created by Nathalia Neves on 13/04/24.
+//
+
+import UIKit
+
+class EditProfileUserViewController: UIViewController {
+
+    @IBOutlet weak var aboutLabel: UILabel!
+    @IBOutlet weak var aboutTextField: UITextField!
+    @IBOutlet weak var interestLabel: UILabel!
+    @IBOutlet weak var interestTextField: UITextField!
+    @IBOutlet weak var lifestyleLabel: UILabel!
+    @IBOutlet weak var lifestyleTextField: UITextField!
+    @IBOutlet weak var moreLabel: UILabel!
+    @IBOutlet weak var moreTextField: UITextField!
+    @IBOutlet weak var viewPhotoLabel: UILabel!
+    @IBOutlet weak var userImage1: UIImageView!
+    @IBOutlet weak var userImage2: UIImageView!
+    @IBOutlet weak var userImage3: UIImageView!
+    @IBOutlet weak var userImage4: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+  
+
+}
