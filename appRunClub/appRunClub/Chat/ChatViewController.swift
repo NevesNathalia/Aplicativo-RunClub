d2238cc92b575ad5798b4e90e25620c928015403
Nathália <nathalianevesdasilva@2005>
2024-04-17 08:44:14 -0400

    create branch Chat

---
+++ appRunClub/appRunClub/Chat/ChatViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatViewController.swift
+//  appRunClub
+//
+//  Created by Nathalia Neves on 17/04/24.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+}
