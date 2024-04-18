c28e54db4181e73f6613980027e939059c5eef13
Nathália <nathalianevesdasilva@2005>
2024-04-18 08:04:39 -0400

    create branch newGroup

---
+++ appRunClub/appRunClub/newGroup/NewGroupViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewGroupViewController.swift
+//  appRunClub
+//
+//  Created by Nathalia Neves on 18/04/24.
+//
+
+import UIKit
+
+class NewGroupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+}
