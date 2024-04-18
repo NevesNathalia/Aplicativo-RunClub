ef677f6e612f1f48b0c3ab1993eefb053d049dfb
Nathália <nathalianevesdasilva@2005>
2024-04-18 14:08:39 -0400

    create tableViewCell da screen newGroup

---
+++ appRunClub/appRunClub/newGroup/Cell/Contacts/ContactsTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsTableViewCell.swift
+//  appRunClub
+//
+//  Created by Nathalia Neves on 18/04/24.
+//
+
+import UIKit
+
+class ContactsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       
+    }
+    
+}
