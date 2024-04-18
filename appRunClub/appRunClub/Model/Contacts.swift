1de40a920bc911e18313d9f7a1afc65bd6349a3e
Nathália <nathalianevesdasilva@2005>
2024-04-18 14:10:27 -0400

    create model

---
+++ appRunClub/appRunClub/Model/Contacts.swift
@@ -0,0 +1,16 @@
+//
+//  Contacts.swift
+//  appRunClub
+//
+//  Created by Nathalia Neves on 18/04/24.
+//
+
+import Foundation
+import UIKit
+
+struct Contacts {
+    var image: UIImage
+    var name: String
+    var isEnable: Bool
+}
+
