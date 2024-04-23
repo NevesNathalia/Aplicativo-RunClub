218a9005597ac2b1c32473a4bb902e8373469690
Nathália <nathalianevesdasilva@2005>
2024-04-23 08:53:00 -0400

    fazendo conexoes dos elementos

--- appRunClub/appRunClub/CommunityGroup/CommunityGroupViewController.swift
+++ appRunClub/appRunClub/CommunityGroup/CommunityGroupViewController.swift
@@ -9,21 +9,20 @@ import UIKit
 
 class CommunityGroupViewController: UIViewController {
 
+    
+    @IBOutlet weak var viewTop: UIView!
+    @IBOutlet weak var personImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var viewBottom: UIView!
+    @IBOutlet weak var mensageTextField: UITextField!
+    @IBOutlet weak var toSendButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+      
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
