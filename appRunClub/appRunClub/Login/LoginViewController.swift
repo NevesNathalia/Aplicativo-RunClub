1b0890744769da3492456a4e066314977cd0936d
Nathália <nathalianevesdasilva@2005>
2024-05-11 21:24:45 -0400

    no mensage

--- appRunClub/appRunClub/Login/LoginViewController.swift
+++ appRunClub/appRunClub/Login/LoginViewController.swift
@@ -6,27 +6,21 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var logInLabel: UILabel!
-    
     @IBOutlet weak var emailLabel: UILabel!
-    
     @IBOutlet weak var emailTextField: UITextField!
-    
     @IBOutlet weak var passwordLabel: UILabel!
-    
     @IBOutlet weak var passwordTextField: UITextField!
-    
     @IBOutlet weak var passwordForgotButton: UIButton!
-    
     @IBOutlet weak var submitButton: UIButton!
-    
     @IBOutlet weak var dontHaveAnAccountLabel: UILabel!
-    
     @IBOutlet weak var toSignUpButton: UIButton!
-    
     @IBOutlet weak var viewVerde: UIView!
     
     override func viewWillAppear(_ animated: Bool) {
@@ -40,8 +34,7 @@ class LoginViewController: UIViewController {
         configTextFields()
         configView(viewVerde, backgroundColor: UIColor(red: 109/255, green: 181/255, blue: 139/255, alpha: 1.0), cornerRadius: 60.0, borderWidth: 0.5, borderColor: CGColor(red: 109/255, green: 181/255, blue: 139/255, alpha: 1.0))
     }
-    
-    
+
     func configLabels() {
         // Text
         logInLabel.text = "Log In"
@@ -68,7 +61,6 @@ class LoginViewController: UIViewController {
         passwordTextField.keyboardType = .default
         // Security password
         passwordTextField.isSecureTextEntry = true
-        
     }
     
     func configButtons() {
@@ -86,8 +78,6 @@ class LoginViewController: UIViewController {
         submitButton.layer.borderWidth = 1.0
         submitButton.tintColor = UIColor(red: 109/255, green: 181/255, blue: 139/255, alpha: 1.0)
         submitButton.layer.borderColor = CGColor(red: 109/255, green: 181/255, blue: 139/255, alpha: 1.0)
-        
-            
     }
     
     func configView(_ view: UIView, backgroundColor: UIColor, cornerRadius: Double, borderWidth: Double, borderColor: CGColor) {
@@ -101,14 +91,12 @@ class LoginViewController: UIViewController {
         let resetPasword = UIStoryboard(name: String(describing: ResetPasswordViewController.self), bundle: nil).instantiateViewController(withIdentifier: String(describing: ResetPasswordViewController.self)) as? ResetPasswordViewController
         
         navigationController?.pushViewController(resetPasword ?? UIViewController(), animated: true)
-        
     }
     
     @IBAction func tappedSubmitButton(_ sender: UIButton) {
         let tabBar = UIStoryboard(name: String(describing: TabBarController.self), bundle: nil).instantiateViewController(withIdentifier: String(describing: TabBarController.self)) as? TabBarController
         
         navigationController?.pushViewController(tabBar ?? UIViewController(), animated: true)
-        
     }
     
     @IBAction func tappedToSignUpButton(_ sender: UIButton) {
@@ -117,4 +105,11 @@ class LoginViewController: UIViewController {
 //
 //        navigationController?.pushViewController(paraSignUp, animated: true)
     }
+    
+//    private var auth = Auth.auth()
+//    
+//     Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+//      guard let strongSelf = self else { return }
+//      // ...
+//    }
 }
